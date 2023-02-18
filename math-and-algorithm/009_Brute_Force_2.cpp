09d0eee19af5c2403a079f583dbb4e64e28986da
applejxd <9639592+applejxd@users.noreply.github.com>
2023-02-18 14:52:38 +0900

    update

--- math-and-algorithm/009_Brute_Force_2.cpp
+++ math-and-algorithm/009_Brute_Force_2.cpp
@@ -1,20 +1,46 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
+
+using namespace std;
 
 int main() {
-  int N, S;
-  std::cin >> N >> S;
+  /** 入力 */
 
-  std::vector<int> A;
+  int N, S;
+  cin >> N >> S;
+
+  vector<int> A;
   for (int idx = 0; idx < N; ++idx) {
     int tmp;
-    std::cin >> tmp;
+    cin >> tmp;
     // 目標値より低い値のみ保存
     if (tmp > S) continue;
     A.push_back(tmp);
   }
 
-  
+  // 動的計画法
+  // A_1~A_i までの数字による和で得られるパターンを保存して
+  // A_{i+1} までの数字による和で得られるパターンを計算
+
+  vector<int> candidates = {0};
+  for (auto &&a : A) {
+    vector<int> new_candidates;
+
+    for (auto &&elem : candidates) {
+      new_candidates.push_back(elem);
+
+      int tmp = elem + a;
+      // 目標値より低い値のみ保存
+      if (tmp < S) {
+        new_candidates.push_back(tmp);
+      } else if (tmp == S) {
+        cout << "Yes";
+        return 0;
+      }
+    }
+    candidates = new_candidates;
+  }
+
+  cout << "No";
 
   return 0;
 }
