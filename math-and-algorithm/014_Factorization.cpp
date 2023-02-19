90a5555b65d63661725898929e7e2d1760b34863
applejxd <9639592+applejxd@users.noreply.github.com>
2023-02-19 09:37:08 +0900

    update

--- math-and-algorithm/014_Factorization.cpp
+++ math-and-algorithm/014_Factorization.cpp
@@ -2,24 +2,54 @@
 
 using namespace std;
 
-vector<int> get_factors(long long N) {
-  vector<int> factors;
-  for (int i = 2; i <= sqrt(N); i++) {
-    if (N % i == 0) {
-      factors.push_back(i);
-      factors.push_back(N / i);
+/**
+ * エラトステネスの篩
+ */
+vector<int> soe(long long N) {
+  // [0,N-1] -> [1,N]
+  vector<bool> is_prime(N, true);
+  is_prime.at(0) = false;
+
+  for (int sieve = 2; sieve <= sqrt(N); ++sieve) {
+    // 篩が素数でなければスキップ
+    if (!is_prime.at(sieve - 1)) continue;
+
+    // 篩をかける
+    for (int times = 2; times <= N / sieve; ++times) {
+      is_prime.at(sieve * times - 1) = false;
     }
   }
-  return factors;
-}
 
-vector<int> factorization(vector<int> factors) {
-  const auto new_factors = factorization(Z)
+  vector<int> primes;
+  // [1,N] -> [0,N-1]
+  for (int i = 0; i < N; i++) {
+    if (is_prime.at(i)) {
+      primes.push_back(i + 1);
+    }
+  }
+
+  return primes;
 }
 
 int main() {
   long long N;
   cin >> N;
+
+  // 素数(因数)判定はsqrt(N) まで行えばいい
+  const auto primes = soe(sqrt(N));
+
+  vector<int> factors;
+  for (auto &&prime : primes) {
+    while (fmod(N, prime) == 0) {
+      factors.push_back(prime);
+      N /= prime;
+    }
+  }
+  if (N != 1) factors.push_back(N);
+
+  for (auto &&factor : factors) {
+    cout << factor << " ";
+  }
 
   return 0;
 }
