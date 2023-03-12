ba2331cee2b772d247addbc56aa4389b43ab4364
applejxd <9639592+applejxd@users.noreply.github.com>
2023-03-12 13:31:06 +0900

    update

--- config/activate.zsh
+++ config/activate.zsh
@@ -24,87 +24,87 @@ function ojt() {
   rm -rf test
 }
 
-#-------#
-# build #
-#-------#
+# #-------#
+# # build #
+# #-------#
 
-function runcpp() {
-  fname=$(echo "$1" | awk -F/ '{print $NF}' | awk -F. '{print $1}')
-  g++ -std=gnu++17 -Wall -Wextra -O2 "$1" -o "$fname"
-  shift
-  ./"$fname" "$@"
-}
-alias -s {c,cc,cpp}='runcpp'
+# function runcpp() {
+#   fname=$(echo "$1" | awk -F/ '{print $NF}' | awk -F. '{print $1}')
+#   g++ -std=gnu++17 -Wall -Wextra -O2 "$1" -o "$fname"
+#   shift
+#   ./"$fname" "$@"
+# }
+# alias -s {c,cc,cpp}='runcpp'
 
-#-------#
-# zinit #
-#-------#
+# #-------#
+# # zinit #
+# #-------#
 
-### Added by Zinit's installer
-if [[ ! -f $HOME/.zinit/bin/zinit.zsh ]]; then
-    print -P "%F{33}▓▒░ %F{220}Installing DHARMA Initiative Plugin Manager (zdharma/zinit)…%f"
-    command mkdir -p "$HOME/.zinit" && command chmod g-rwX "$HOME/.zinit"
-    command git clone https://github.com/zdharma-continuum/zinit "$HOME/.zinit/bin" && \
-        print -P "%F{33}▓▒░ %F{34}Installation successful.%f%b" || \
-        print -P "%F{160}▓▒░ The clone has failed.%f%b"
-fi
+# ### Added by Zinit's installer
+# if [[ ! -f $HOME/.zinit/bin/zinit.zsh ]]; then
+#     print -P "%F{33}▓▒░ %F{220}Installing DHARMA Initiative Plugin Manager (zdharma/zinit)…%f"
+#     command mkdir -p "$HOME/.zinit" && command chmod g-rwX "$HOME/.zinit"
+#     command git clone https://github.com/zdharma-continuum/zinit "$HOME/.zinit/bin" && \
+#         print -P "%F{33}▓▒░ %F{34}Installation successful.%f%b" || \
+#         print -P "%F{160}▓▒░ The clone has failed.%f%b"
+# fi
 
-source "$HOME/.zinit/bin/zinit.zsh"
-autoload -Uz _zinit
-(( ${+_comps} )) && _comps[zinit]=_zinit
-### End of Zinit's installer chunk
+# source "$HOME/.zinit/bin/zinit.zsh"
+# autoload -Uz _zinit
+# (( ${+_comps} )) && _comps[zinit]=_zinit
+# ### End of Zinit's installer chunk
 
-# fish-like auto completion
-zinit light zsh-users/zsh-autosuggestions
-# completion for non-defalut commands
-zinit light zsh-users/zsh-completions
-# Fish like interactive tab completion for cd in zsh
-zinit ice pick"zsh-interactive-cd.plugin.zsh" wait'!0'
-zinit light changyuheng/zsh-interactive-cd
-# syntax-highlighting to command-line (after compinit)
-zinit ice wait'!0'
-zinit light zsh-users/zsh-syntax-highlighting
+# # fish-like auto completion
+# zinit light zsh-users/zsh-autosuggestions
+# # completion for non-defalut commands
+# zinit light zsh-users/zsh-completions
+# # Fish like interactive tab completion for cd in zsh
+# zinit ice pick"zsh-interactive-cd.plugin.zsh" wait'!0'
+# zinit light changyuheng/zsh-interactive-cd
+# # syntax-highlighting to command-line (after compinit)
+# zinit ice wait'!0'
+# zinit light zsh-users/zsh-syntax-highlighting
 
-# Pure Theme
-zinit ice compile'(pure|async).zsh' pick'async.zsh' src'pure.zsh'
-zinit light sindresorhus/pure
-zinit ice from"gh-r" as"program"
-zinit load junegunn/fzf-bin
+# # Pure Theme
+# zinit ice compile'(pure|async).zsh' pick'async.zsh' src'pure.zsh'
+# zinit light sindresorhus/pure
+# zinit ice from"gh-r" as"program"
+# zinit load junegunn/fzf-bin
 
-# fzf
-zinit ice multisrc'shell/(key-bindings|completion).zsh'
-zinit load junegunn/fzf
-# z
-zinit ice pick"z.sh"
-zinit light rupa/z
+# # fzf
+# zinit ice multisrc'shell/(key-bindings|completion).zsh'
+# zinit load junegunn/fzf
+# # z
+# zinit ice pick"z.sh"
+# zinit light rupa/z
 
-#-----#
-# zsh #
-#-----#
+# #-----#
+# # zsh #
+# #-----#
 
-# cd completion
-# cf. http://bit.ly/2ZtPPrN
-setopt auto_cd
-cdpath=(.. ~)
-function chpwd() {
-  if type "lsd" >/dev/null 2>&1; then
-    lsd
-  else
-    ls
-  fi
-}
+# # cd completion
+# # cf. http://bit.ly/2ZtPPrN
+# setopt auto_cd
+# cdpath=(.. ~)
+# function chpwd() {
+#   if type "lsd" >/dev/null 2>&1; then
+#     lsd
+#   else
+#     ls
+#   fi
+# }
 
-# z-fzf, emacs-like key-bindings
-# cf. http://bit.ly/2sEPZAJ
-if type "z" >/dev/null 2>&1; then
-    function z-fzf() {
-        local selected_dir=$(_z -l 2>&1 | fzf +s --tac | sed 's/^[0-9,.]* *//')
-        if [[ -n "$selected_dir" ]]; then
-            BUFFER="cd ${selected_dir}"
-            zle accept-line
-        fi
-        zle reset-prompt
-    }
-    zle -N z-fzf
-    bindkey "^X^F" z-fzf
-fi
+# # z-fzf, emacs-like key-bindings
+# # cf. http://bit.ly/2sEPZAJ
+# if type "z" >/dev/null 2>&1; then
+#     function z-fzf() {
+#         local selected_dir=$(_z -l 2>&1 | fzf +s --tac | sed 's/^[0-9,.]* *//')
+#         if [[ -n "$selected_dir" ]]; then
+#             BUFFER="cd ${selected_dir}"
+#             zle accept-line
+#         fi
+#         zle reset-prompt
+#     }
+#     zle -N z-fzf
+#     bindkey "^X^F" z-fzf
+# fi
