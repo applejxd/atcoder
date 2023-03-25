ef4ef5a51b442dc0c667a699604c67118dd5a848
applejxd <9639592+applejxd@users.noreply.github.com>
2023-03-25 13:52:03 +0900

    update

---
+++ src/math-and-algorithm/Chapter4/043_Depth_First_Search.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+
+using namespace std;
+using namespace atcoder;
+using uint = unsigned int;
+using ll = long long;
+using ull = unsigned long long;
+
+int main() {
+  ll N, M;
+  cin >> N >> M;
+  vector<ll> A(M), B(M);
+  for (long long i = 0; i < M; i++) {
+    cin >> A[i] >> B[i];
+  }
+
+    return 0;
+}
